55827764990ed0c51b3c0651eaad90af8f8ec08d
anapau-u <ana.villa.ch@usb.edu.mx>
2022-07-28 09:24:47 -0500

    update 9:20

--- registrousuario-form.php
+++ registrousuario-form.php
@@ -29,7 +29,7 @@
             Registro de clientes!
           </span>
 <?php
-    $serverName = "192.168.100.52, 1433";
+    $serverName = "172.16.22.106, 1433";
     $connectionInfo = array("Database"=>"JAAPA", "UID"=>"JAAPAPAM", "PWD"=>"123");
 
     $varuser = $_POST["usuario"];
